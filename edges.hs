47d5c40fda4f3b6eecbb65d5bc33ae462d72ae8a
Pedro Juan Soto <psoto004@fiu.edu>
2019-04-18 01:22:16 -0400

    2nd commit (got rid of some old commented out code and fixed an error in edges to num)

--- edges.hs
+++ edges.hs
@@ -1,20 +1,3 @@
-{-
-edges_to_num :: [Int] -> Int
-edges_to_num [0,1] = 0
-edges_to_num [x,y] = (x+ (y * (y-1))) `div` 2
-
-edges :: Int -> (Int,Int)
-edges 0 = (0,1)
-edges n = if  ((snd (edges (n-1))) - (fst (edges (n-1)))) == 1 then (0,(snd (edges (n-1)))+1) else ((fst (edges (n-1))) +1 , snd (edges (n-1)))
-
-vertices n = [0..n-1]
-
-graph n = map edges (vertices n)
-
-adjacent :: (Int,Int) -> (Int,Int) -> Bool
-adjacent x y = if (fst x == fst y) || (snd x == snd y)  || (fst x == snd y) || (snd x == fst y) then True else False
--}
-
 type Vertex  = ([Int],[Int],[Int])
 
 getredlist :: Vertex -> [Int]
@@ -38,15 +21,12 @@ create_vertex_set n = (map add_two_empty (map (append (n-1)) (map getuncolored (
 
 type Edge = (Vertex,Vertex)
 
-
 simple_edges :: Int -> (Int,Int)
 simple_edges 0 = (0,1)
 simple_edges n = if  ((snd (simple_edges (n-1))) - (fst (simple_edges (n-1)))) == 1 then (0,(snd (simple_edges (n-1)))+1) else ((fst (simple_edges (n-1))) +1 , snd (simple_edges (n-1)))
 
-
 edges ::  [Vertex] -> Int -> Edge
 edges vs n = ( vs !! fst (simple_edges n) , vs !! snd (simple_edges n))
-
 
 type Graph = ([Vertex],[Edge])
 
@@ -55,7 +35,11 @@ create_Graph n = (create_vertex_set n , map (edges (create_vertex_set n)) [0..((
 
 edges_to_num :: (Int,Int) -> Int
 edges_to_num (0,1) = 0
-edges_to_num (x,y) = (x+ (y * (y-1))) `div` 2
+edges_to_num (x,y) = x+ (((y * (y-1))) `div` 2)
+
+insert :: [Int] -> Int -> [Int]
+insert [] x = [x]
+insert (y:ys) x = if x < y then x : y : ys else y : insert ys x
 
 vertex_color_edge_update :: Vertex -> Int -> Vertex
 vertex_color_edge_update v i = v
